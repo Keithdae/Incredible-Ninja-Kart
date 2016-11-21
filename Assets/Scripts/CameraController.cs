53f75be1ad65ed268f8a31c429e86f7e94005745
WOLFROM Matthieu <keithdae@gmail.com>
2016-11-21 22:57:26 +0100

    Nouvelle caméra, roues kart mieux localisées

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject target; // l’objet à suivre
+	private Vector3 offset; // L’offset initial
+
+	void Start ()
+	{
+		offset = transform.position - target.transform.position;
+		offset.x = 0;
+		offset.z = 0;
+	}
+
+	void LateUpdate()
+	{
+		transform.position = target.transform.position + offset + 10 * (new Vector3(-target.transform.forward.x, 0, -target.transform.forward.z));
+		transform.LookAt (target.transform);
+	}
+}
