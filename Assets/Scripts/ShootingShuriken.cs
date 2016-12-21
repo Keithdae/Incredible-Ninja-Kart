e8d422983e6fc052100873e5e1275006d85e3c44
Daniel Antunes Costa Gonçalves <dantunescost92@gmail.com>
2016-12-21 13:55:59 +0100

    tir avec raycast (mais collisions ne marchent plus?)

--- Assets/Scripts/ShootingShuriken.cs
+++ Assets/Scripts/ShootingShuriken.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ShootingShuriken : MonoBehaviour {
@@ -6,7 +7,10 @@ public class ShootingShuriken : MonoBehaviour {
     public GameObject shuriken;
     public int Nombre_de_Shurikens;
     public int shootingForce;
-    public Transform shurikenTransform;
+    public Transform shurikenSpawn;
+    public Image aimImage;
+    public Camera camera;
+    public float shurikenRange;
 
     private string fireButton;
     private GameObject[] munitions;
@@ -21,7 +25,7 @@ public class ShootingShuriken : MonoBehaviour {
         munitions = new GameObject[Nombre_de_Shurikens];
         for(int i = 0; i < Nombre_de_Shurikens; i++)
         {
-            munitions[i] = (GameObject)Instantiate(shuriken, shurikenTransform.position, shurikenTransform.rotation);
+            munitions[i] = (GameObject)Instantiate(shuriken, shurikenSpawn.position, shurikenSpawn.rotation);
             munitions[i].GetComponent<Transform>().localScale = new Vector3(1.5f, 1.5f, 1.5f);
             munitions[i].layer = transform.gameObject.layer;
             munitions[i].SetActive(false);
@@ -31,20 +35,31 @@ public class ShootingShuriken : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(fireButton))
+        if (this.gameObject.tag == "Player")
         {
-            Fire();
+            aimImage.rectTransform.position = Input.mousePosition;
+            Ray aimingRay = camera.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(aimingRay.origin, aimingRay.direction * 200, Color.yellow);
+            if (Input.GetButtonDown(fireButton))
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(aimingRay, out hit, shurikenRange))
+                {
+                    Vector3 shootingDirection = hit.transform.position - shurikenSpawn.position;
+                    Fire(shootingDirection);
+                }
+            }
         }
     }
 
-    private void Fire()
+    private void Fire(Vector3 shootDir)
     {
         // on active le shuriken et on le tire
         if(munitions[currentShuriken].activeSelf == false)
         {
+            munitions[currentShuriken].GetComponent<Transform>().position = shurikenSpawn.position;
+            munitions[currentShuriken].GetComponent<Rigidbody>().velocity = shootingForce * shootDir;
             munitions[currentShuriken].SetActive(true);
-            munitions[currentShuriken].GetComponent<Transform>().position = shurikenTransform.position;
-            munitions[currentShuriken].GetComponent<Rigidbody>().velocity = shootingForce * transform.forward;
 
             currentShuriken = (currentShuriken + 1) % Nombre_de_Shurikens;
         }
